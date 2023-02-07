c7df88e441467ef43ccd0d55e983e254e0576b29
anuragse2910 <102716252+anuragse2910@users.noreply.github.com>
2023-02-07 22:27:17 +0530

    Add files via upload

---
+++ Task_1.py
@@ -0,0 +1,11 @@
+from plyer import notification
+
+title = 'Hello Amazing people!'
+
+message= 'Thankyou for reading! Take care!'
+
+notification.notify(title= title,
+                    message= message,
+                    app_icon = None,
+                    timeout= 10,
+                    toast=False)
